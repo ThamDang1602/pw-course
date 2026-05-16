8cdbd0133decdb728029cb7b45f8676518f76484
ThamDH <thamk55tha@gmail.com>
2026-05-16 16:31:03 +0700

    feat: add homework2

---
+++ tests/lesson-02/ex2.js
@@ -0,0 +1,10 @@
+let myName = "Alex";
+myName = "Nagi";
+console.log ("Đổi được tên nè: " + myName);
+
+var myName1 = "ThamDH";
+myName1 = "Camelia";
+console.log ("Đổi được tên nè:", myName1);
+
+const myName3 = "MyMy"
+console.log ("Không đổi được tên đâu, vẫn là tên ban đầu nè: ",myName3);
---
+++ tests/lesson-02/ex3.js
@@ -0,0 +1,13 @@
+a = true;
+b = false;
+c = true;
+
+Câu 1: a && b && c
+a && b && c = true VÀ false VÀ true
+            = false VÀ true
+            = false
+
+Câu 2: a && b || c
+a && b || c = true VÀ false HOẶC false
+            = false HOẶC true
+            = true
